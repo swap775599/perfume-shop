9023421d40a916cef78d4078d4971e729484654c
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-10-08 12:57:32 +0530

    Create reviewRoutes.js

---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,9 @@
+// routes/reviewRoutes.js
+const express = require('express');
+const { getReviews } = require('../controllers/reviewController');
+
+const router = express.Router();
+
+router.get('/', getReviews);
+
+module.exports = router;
