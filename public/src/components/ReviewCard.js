75095144c42e3b97c5dc97b598523750e2dbbcd9
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-10-08 12:39:08 +0530

    Update ReviewCard.js

--- public/src/components/ReviewCard.js
+++ public/src/components/ReviewCard.js
@@ -1,1 +1,11 @@
 
+import React from 'react';
+
+const ReviewCard = ({ review }) => (
+  <div className="review-card">
+    <p>"{review.text}"</p>
+    <h4>{review.name}, {review.location}</h4>
+  </div>
+);
+
+export default ReviewCard;
