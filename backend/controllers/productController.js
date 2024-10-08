d82853d7097ea2c9842aa13a8d74f74745f444e5
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-10-08 12:54:05 +0530

    Create productController.js

---
+++ backend/controllers/productController.js
@@ -0,0 +1,11 @@
+// controllers/productController.js
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
