31d49b1dd5a1734df77fa464d70d0de60d3de04d
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-10-08 12:44:55 +0530

    Create About.js

---
+++ public/src/pages/About.js
@@ -0,0 +1,16 @@
+// src/pages/About.js
+import React from 'react';
+import './About.css'; // Optional: Add CSS for styling
+
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>
+      Welcome to our Perfume Shop! We offer a curated selection of high-quality perfumes
+      sourced from around the world. Our mission is to provide you with the finest fragrances
+      to elevate your everyday life.
+    </p>
+  </div>
+);
+
+export default About;
